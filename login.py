397b782180cd093460afeaf8fda0c44172635a60
Janna Zaman <111919098+jannazaman@users.noreply.github.com>
2023-10-02 02:32:57 -0500

    Update login.py

--- login.py
+++ login.py
@@ -3,21 +3,21 @@ from tkinter import messagebox
 from PIL import ImageTk
 
 def login():
+    # Check if username or password is empty
     if usernameEntry.get() == '' or passwordEntry.get() == '':
         messagebox.showerror('Error', 'Both username and password are required fields.')
+    # Check if the entered username and password are correct
     elif usernameEntry.get() == 'Janna' and passwordEntry.get() == '12345':
-        # Use wraplength to set the width of the messagebox
+        # Show a successful login message
         messagebox.showinfo('Approved', 'Welcome! Login Successful.')
-
-        # Close login window when sms window opens
+        # Close the login window when successful and open another window (sms.py)
         window.destroy()
         import sms
-
     else:
+        # Show an error message for invalid credentials
         messagebox.showerror('Invalid Credentials', 'Please enter correct username and password.')
 
 window = Tk()
-
 # Width & Height of window (+0+0 is dist from x & y-axis)
 window.geometry('1280x700+0+0')
 window.title('Login System')
@@ -43,7 +43,6 @@ usernameLabel.grid(row=1, column=0, pady=10, padx=10)
 usernameEntry = Entry(loginFrame, font=('times new roman', 20, 'bold'), bd=5, fg='medium aquamarine')
 usernameEntry.grid(row=1, column=1, pady=10, padx=10)
 
-
 passwordImage = PhotoImage(file='password.png')
 passwordLabel = Label(loginFrame, image=passwordImage, text='Password', compound=LEFT
                       , font=('times new roman', 20, 'bold'), bg='azure3')
@@ -56,6 +55,5 @@ loginButton = Button(loginFrame, text='Login', font=('times new roman', 15, 'bol
                      , fg='white', bg='cadet blue', activebackground='cadet blue'
                      , activeforeground='white', cursor='hand2', command=login)
 loginButton.grid(row=3, column=1, pady=10)
-
 # .mainloop will keep window on loop, so we can see it continuously
 window.mainloop()
